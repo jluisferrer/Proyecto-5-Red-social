ebef7dd5256ff2434c4a7046c3624d65100d7e2f
jluisferrer <jluisferrer@gmail.com>
2024-03-11 21:10:21 +0100

    feat:UpdatepostbyId controller running

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,11 +1,11 @@
 import Post from "../models/Post.js";
 
-export const newPost = async (req, res)=>{
+export const newPost = async (req, res) => {
     try {
         const userId = req.tokenData.userId
         const description = req.body.description.trim()
 
-        if(!description){
+        if (!description) {
             return res.status(400).json(
                 {
                     success: false,
@@ -18,34 +18,68 @@ export const newPost = async (req, res)=>{
             description
         })
         res.status(201).json({
-			success: true,
-			message: "New post succesfully",
-			token: postNew
-		})
+            success: true,
+            message: "New post succesfully",
+            token: postNew
+        })
     } catch (error) {
         res.status(500).json({
-			success: false,
-			message: "Post could not be created",
-			error: error
-		})
-        
+            success: false,
+            message: "Post could not be created",
+            error: error
+        })
+
     }
 }
 
-export const deletePostById = async (req,res)=>{
+export const deletePostById = async (req, res) => {
     try {
-        const postId = req.params.id 
-        const deletedPost = await Post.findOneAndDelete(postId)   
+        const postId = req.params.id
+        const deletedPost = await Post.findOneAndDelete(postId)
         res.status(201).json({
-			success: true,
-			message: "Post successfully deleted",
-			token: deletedPost
-		})
+            success: true,
+            message: "Post successfully deleted",
+            token: deletedPost
+        })
     } catch (error) {
         res.status(500).json({
-			success: false,
-			message: "Post could not be deleted",
-			error: error
-		})
+            success: false,
+            message: "Post could not be deleted",
+            error: error
+        })
+    }
+}
+
+export const updatePostById = async (req, res) => {
+    try {
+        const postId = req.params.id
+        const newDescription = req.body.description
+
+        if (!newDescription) {
+            res.status(400).json({
+                success: false,
+                message: "There are no changes to the post"
+            })
+        }
+        const modifiedPost = await Post.findByIdAndUpdate
+        (postId,
+         {
+            description: newDescription,
+        },
+        {
+            new: true
+        })
+
+        res.status(201).json({
+            success: true,
+            message: "Post successfully updated",
+            data: modifiedPost
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "The post is not found",
+            error: error
+        })
     }
 }
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { newPost, deletePostById } from "../controllers/post.controller.js";
+import { newPost, deletePostById, updatePostById } from "../controllers/post.controller.js";
 import { auth } from "../middlewares/auth.js";
 
 const router = Router();
 
 router.post('/',auth, newPost)
 router.delete('/:id',auth, deletePostById)
+router.put('/:id',auth, updatePostById)
 
 
 export default router;
