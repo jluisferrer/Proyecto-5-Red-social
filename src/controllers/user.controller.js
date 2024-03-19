e7b9f00de6c8c4c23a9ecaf69141d7881ddfe560
jluisferrer <jluisferrer@gmail.com>
2024-03-19 19:15:01 +0100

    update:formating code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import User from "../models/User.js";
 export const getUsers = async (req, res) => {
     try {
         const users = await User.find()
-            res.status(200).json({
+        res.status(200).json({
             success: true,
             message: `Total of ${users.length} users found.`,
             data: users
