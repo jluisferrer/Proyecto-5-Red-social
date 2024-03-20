5fc2ba3aabba33b3c6c0f519fd63aa419742e925
jluisferrer <jluisferrer@gmail.com>
2024-03-20 20:00:10 +0100

    update:adding cors library to server

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import 'dotenv/config';
 import { dbConnection } from "./database/db.js";
 import router from "./routes/router.js";
+import cors from "cors";
 
 const app = express()
 
 // parsea el body para que sea undefined
+app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT ||4004;
